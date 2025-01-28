4ed80c5c1e9f38b1411f33bc8a915a3dd4219cfa
Landon-Dougherty <dougherty.l@stu.janesville.k12.wi.us>
2025-01-28 21:07:03 +0000

    Update 1/28

---
+++ hello.py
@@ -0,0 +1,19 @@
+print("Hello, world!")
+# This is a single-line comment
+""" Data Types: 
+int     - Integer (Whole Number)
+float   - Floating-point number (number w/decimal)
+str     - String (any text, in quotes)
+chr     - Character (single keyboard character, in singe-quotes)
+bool    - Boolean (either True or False, only)
+"""
+
+""" Arithmetic Operators:
++   Add
+-   Subtract
+*   Multiply 
+/   Divide 
+//  Floor Divide (Divide and round down to nearest Int)
+%   Mod (Remained from Int division)
+**  Power-Of Operator
+"""
