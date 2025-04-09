482d766f2dda206eb989847ad1e4f206a1756be7
Landon-Dougherty <dougherty.l@stu.janesville.k12.wi.us>
2025-04-09 15:24:27 -0500

    4/9/25

---
+++ CSharp/FavClubs/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("press 1 for input code or 2 for just printed code")
+int test = int.Parse(Console.ReadLine());
+if (test == 1)
+{
+    Console.WriteLine("Enter Your Favorite Clubs : ");
+    string clubs = Console.ReadLine();
+    Console.WriteLine("Your Clubs Are")
+}
+if (test == 2)
+{
+    Console.WriteLine("My Favorite Clubs Are... Track, Jazz, and thats literally it.");
+}
---
+++ CSharp/Fullname/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter MY Name... : ");
+string test = Console.ReadLine();
+Console.WriteLine("MY Name Is : " + test);
+
+Console.ReadLine();
